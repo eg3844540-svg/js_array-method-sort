9fdbd3b2f7f4d2372bf5b5d23b0618db67733a92
Erica <eg3844540@gmail.com>
2026-04-25 21:33:40 -0300

    fix: methodSort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,31 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  Array.prototype.sort2 = function (compareFunction) {
+    const arr = this;
+
+    const defaultCompare = (a, b) => {
+      const strA = String(a);
+      const strB = String(b);
+
+      if (strA > strB) return 1;
+      if (strA < strB) return -1;
+      return 0;
+    };
+
+    const compare = compareFunction || defaultCompare;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = 0; j < arr.length - 1 - i; j++) {
+        if (compare(arr[j], arr[j + 1]) > 0) {
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+
+    return arr;
   };
 }
 
